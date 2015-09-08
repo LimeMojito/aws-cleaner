0ed40d9225e0abe42df1bea1937f4b9c865a1b9e
lachlan <lachlan.odonnell@limemojito.com>
2015-09-08 12:44:09 +1000

    Fix comments

--- src/test/java/com/limemojito/aws/cleaner/MainTest.java
+++ src/test/java/com/limemojito/aws/cleaner/MainTest.java
@@ -1,10 +1,12 @@
-package com.limemojito.aws.cleaner;/*
+/*
  * Copyright (c) Lime Mojito Pty Ltd 2011-2015
  *
  * Except as otherwise permitted by the Copyright Act 1967 (Cth) (as amended from time to time) and/or any other
  * applicable copyright legislation, the material may not be reproduced in any format and in any way whatsoever
  * without the prior written consent of the copyright owner.
  */
+
+package com.limemojito.aws.cleaner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
