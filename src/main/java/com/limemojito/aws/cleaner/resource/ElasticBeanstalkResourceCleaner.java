024355abb31ff6e16a606998d03ea127605107c4
lachlan <lachlan.odonnell@limemojito.com>
2023-09-13 14:59:33 +1000

    FX2-323: elastic beanstalk not supported in ap-southeast-4

--- src/main/java/com/limemojito/aws/cleaner/resource/ElasticBeanstalkResourceCleaner.java
+++ src/main/java/com/limemojito/aws/cleaner/resource/ElasticBeanstalkResourceCleaner.java
@@ -17,21 +17,24 @@
 
 package com.limemojito.aws.cleaner.resource;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalk;
 import com.amazonaws.services.elasticbeanstalk.model.DescribeEnvironmentsResult;
 import com.amazonaws.services.elasticbeanstalk.model.EnvironmentDescription;
 import com.amazonaws.services.elasticbeanstalk.model.TerminateEnvironmentRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class ElasticBeanstalkResourceCleaner extends PhysicalResourceCleaner {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticBeanstalkResourceCleaner.class);
     private final AWSElasticBeanstalk client;
 
     @Autowired
@@ -42,18 +45,23 @@ public class ElasticBeanstalkResourceCleaner extends PhysicalResourceCleaner {
 
     @Override
     protected List<String> getPhysicalResourceIds() {
-        final DescribeEnvironmentsResult result = client.describeEnvironments();
-        final List<EnvironmentDescription> environments = result.getEnvironments();
-        LOGGER.debug("{} environments found", environments.size());
-        return environments.stream()
-                           .filter(environmentDescription -> environmentDescription.getStatus().equalsIgnoreCase("Ready"))
-                           .map(EnvironmentDescription::getEnvironmentName)
-                           .collect(Collectors.toList());
+        try {
+            final DescribeEnvironmentsResult result = client.describeEnvironments();
+            final List<EnvironmentDescription> environments = result.getEnvironments();
+            log.debug("{} environments found", environments.size());
+            return environments.stream()
+                               .filter(environmentDescription -> environmentDescription.getStatus().equalsIgnoreCase("Ready"))
+                               .map(EnvironmentDescription::getEnvironmentName)
+                               .collect(Collectors.toList());
+        } catch (SdkClientException e) {
+            log.warn("Could not communicate with elastic beanstalk: {}", e.getMessage(),e);
+            return Collections.emptyList();
+        }
     }
 
     @Override
     protected void performDelete(String physicalId) {
-        LOGGER.info("Terminating environment {}", physicalId);
+        log.info("Terminating environment {}", physicalId);
         client.terminateEnvironment(new TerminateEnvironmentRequest().withEnvironmentName(physicalId));
     }
 }
